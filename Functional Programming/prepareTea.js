3a85641269f2c4854bf3cc269941ee3329f9c3eb
Musab1258 <67251181+Musab1258@users.noreply.github.com>
2021-12-17 20:11:32 +0100

    Add files via upload

---
+++ Functional Programming/prepareTea.js
@@ -0,0 +1,12 @@
+const prepareTea = () => "greanTea"
+const getTea = (numOfCups) => {
+  const teaCups = [ ];
+  for (let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea()
+    teaCups.push(teaCup)
+  }
+  return teaCups;
+}
+
+const tea4TeamFCC = getTea(40);
+console.log(tea4TeamFCC)
